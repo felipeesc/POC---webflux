85c09accb12e146beae7f6506751acb29b17c662
Carvalho <felipee_sc@hotmail.com>
2021-12-10 18:30:22 -0300

    refactor controller

--- src/main/java/com/example/pocwebflux/controller/PocController.java
+++ src/main/java/com/example/pocwebflux/controller/PocController.java
@@ -2,7 +2,7 @@ package com.example.pocwebflux.controller;
 
 import com.example.pocwebflux.domain.Poc;
 import com.example.pocwebflux.service.PocService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,10 +12,10 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/v1")
+@RequiredArgsConstructor
 public class PocController {
 
-    @Autowired
-    private PocService pocService;
+    private final PocService pocService;
 
     @GetMapping
     public Flux<Poc> listAll() {
