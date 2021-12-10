08b93ca06117d49d2e806ae440632ed2098165db
Carvalho <felipee_sc@hotmail.com>
2021-12-10 17:34:50 -0300

    add BlochHound

--- src/main/java/com/example/pocwebflux/PocWebfluxApplication.java
+++ src/main/java/com/example/pocwebflux/PocWebfluxApplication.java
@@ -2,10 +2,13 @@ package com.example.pocwebflux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.blockhound.BlockHound;
 
 @SpringBootApplication
 public class PocWebfluxApplication {
-
+    static {
+        BlockHound.install();
+    }
     public static void main(String[] args) {
         SpringApplication.run(PocWebfluxApplication.class, args);
     }
