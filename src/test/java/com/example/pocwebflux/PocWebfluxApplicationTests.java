6203c3b8e0cab5cb778a33c7bd290130d64b1cd3
Carvalho <felipee_sc@hotmail.com>
2021-12-10 16:26:35 -0300

    starting project

---
+++ src/test/java/com/example/pocwebflux/PocWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pocwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
